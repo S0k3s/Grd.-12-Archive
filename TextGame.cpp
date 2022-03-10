870f6e6252d969bde3334353db7ce89bd341583b
S0k3s <94992699+S0k3s@users.noreply.github.com>
2022-03-10 11:43:19 -0500

    Update TextGame.cpp

--- TextGame.cpp
+++ TextGame.cpp
@@ -3,6 +3,7 @@
 #include <fstream>
 #include <windows.h>
 #include "Header.h" // Holding clear function
+#include <array>
 
 using namespace std;
 
@@ -14,6 +15,7 @@ string NewGame() {
 	bool check = true;
 
 	do {
+		check = true;
 		cout << "What should the file be saved as: ";
 		cin >> file; // getting the name of the new file
 
@@ -43,7 +45,7 @@ string NewGame() {
 			ofstream New_File(file); // creating file with inputed name
 			New_File << file << endl << "0\n1\n0\n0\n0\n0\n0";
 
-			cout << "Welcome to Mining Mania!\n\n" << "This game requires no skill, just patience and GPU power.\n" << "The aim of the game is to mine the most gold as you possibly can.\n";
+			cout << "Welcome to Mining Mania!\n\n" << "This game requires no skill, just patience and CPU power.\n" << "The aim of the game is to mine the most gold as you possibly can.\n";
 			cout << "Have fun and just let it run.";
 		}
 		else { cout << "File already exists\n"; }
@@ -84,84 +86,76 @@ string LoadGame() {
 		return file;
 }
 void Print(string OpenFile) {
-	int Current$ = 0;
-	int Picklvl = 0;
-	int Miners = 0;
-	int Trucks = 0;
-	int Dinamite = 0;
-	int Nukes = 0;
-	int FullPower = 0;
+	array<int, 7>info;
 	string line;
 
 	fstream File(OpenFile);
 
 	for (int i = 0; i <= 7; i++) {
 		getline(File, line);
-		if (i == 1) { Current$ = stoi(line); }
-		if (i == 3) { Picklvl = stoi(line); }
-		if (i == 2) { Miners = stoi(line); }
-		if (i == 4) { Trucks = stoi(line); }
-		if (i == 5) { Dinamite = stoi(line); }
-		if (i == 6) { Nukes = stoi(line); }
-		if (i == 7) { FullPower = stoi(line); }
+		if (i == 1) { info[i - 1] = stoi(line); }
+		if (i == 2) { info[i - 1] = stoi(line); }
+		if (i == 3) { info[i - 1] = stoi(line); }
+		if (i == 4) { info[i - 1] = stoi(line); }
+		if (i == 5) { info[i - 1] = stoi(line); }
+		if (i == 6) { info[i - 1] = stoi(line); }
+		if (i == 7) { info[i - 1] = stoi(line); }
 	}
 	File.close();
 
-	cout << "Current $: " << Current$ << endl;
-	cout << "Picklvl: " << Picklvl << endl;
-	cout << "Miners: " << Miners << endl;
-	if (Picklvl < 10) {
+	cout << "Current $: " << info[0] << endl;
+	cout << "Picklvl: " << info[1] << endl;
+	cout << "Miners: " << info[2] << endl;
+	if (info[1] < 10) {
 		cout << "Trucks: " << "Locked until Picklvl 10\n";
 	}
 	else {
-		cout << "Trucks: " << Trucks << endl;
+		cout << "Trucks: " << info[3] << endl;
 	}
-	if (Picklvl < 50) {
+	if (info[1] < 50) {
 		cout << "Dinamite: " << "Locked until Picklvl 50\n";
 	}
 	else {
-		cout << "Dinamite: " << Dinamite << endl;
+		cout << "Dinamite: " << info[4] << endl;
 	}
-	if (Nukes < 100) {
+	if (info[1] < 100) {
 		cout << "Nukes: " << "Locked until Picklvl 100\n";
 	}
 	else {
-		cout << "Nukes: " << Nukes << endl;
+		cout << "Nukes: " << info[5] << endl;
 	}
-	if (FullPower == 1000) {
+	if (info[1] < 1000) {
 		cout << "Full Power: " << "Locked until Picklvl 1000\n";
 	}
 	else {
-		cout << "Full Power: " << FullPower << endl;
+		cout << "Full Power: " << info[6] << endl;
 	}
 }
-void Write(string OpenFile) {
+void Write(string OpenFile, array<int,7>info) {
+	string line;
+
+	fstream File(OpenFile);
+
 
 }
 void Shop(string OpenFile) {
-	int Current$ = 0;
-	int Picklvl = 0;
-	int Miners = 0;
-	int Trucks = 0;
-	int Dinamite = 0;
-	int Nukes = 0;
-	int FullPower = 0;
+	array<int, 7>info; // {Current$, Picklvl, Miners, Trucks, Dinamite, Nukes, FullPower}
 	string input;
 	string upgrade;
-	string amount;
+	int amount;
 	string line;
 
 	fstream File(OpenFile);
 
 	for (int i = 0; i <= 7; i++) {
 		getline(File, line);
-		if (i == 1) { Current$ = stoi(line); }
-		if (i == 3) { Picklvl = stoi(line); }
-		if (i == 2) { Miners = stoi(line); }
-		if (i == 4) { Trucks = stoi(line); }
-		if (i == 5) { Dinamite = stoi(line); }
-		if (i == 6) { Nukes = stoi(line); }
-		if (i == 7) { FullPower = stoi(line); }
+		if (i == 1) { info[i-1] = stoi(line); } // Current$
+		if (i == 2) { info[i-1] = stoi(line); } // Picklvl
+		if (i == 3) { info[i-1] = stoi(line); } // Miners
+		if (i == 4) { info[i-1] = stoi(line); } // Trucks
+		if (i == 5) { info[i-1] = stoi(line); } // Dinamite
+		if (i == 6) { info[i-1] = stoi(line); } // Nukes
+		if (i == 7) { info[i-1] = stoi(line); } // FullPower
 	}
 	File.close();
 
@@ -178,18 +172,29 @@ void Shop(string OpenFile) {
 		cout << "How much do you want to upgrade by? ";
 		cin >> amount;
 
-		if (upgrade == "Picklvl") {}
-		if (upgrade == "Miners") {}
-		if (upgrade == "Trucks") {}
-		if (upgrade == "Dinamite") {}
-		if (upgrade == "Nukes") {}
-		if (upgrade == "FullPower") {}
+		if (upgrade == "Picklvl") { 
+			info[1] += amount; 
+		}
+		if (upgrade == "Miners") { 
+			info[2] += amount; 
+		}
+		if (upgrade == "Trucks") { 
+			info[3] += amount; 
+		}
+		if (upgrade == "Dinamite") { 
+			info[4] += amount; 
+		}
+		if (upgrade == "Nukes") { 
+			info[5] += amount; 
+		}
+		if (upgrade == "FullPower") {
+			info[6] += amount; 
+		}
 	}
 	else if (input == "MINE") {
 
 	}
 }
-
 int main() {
 	string input;
 	string OpenFile;
@@ -211,7 +216,6 @@ int main() {
 		}
 	} while (check == false);
 
-	// Load current stats
 	Shop(OpenFile);
 	
 	return 0;
